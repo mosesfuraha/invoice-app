658ae25184635dbc47d13673065ab47d46e0c487
mosesfuraha <moses.furaha@amalitechtraining.org>
2024-08-28 17:10:17 +0200

    load invoices

--- src/app/pages/invoices/invoice-list/invoice-list.component.ts
+++ src/app/pages/invoices/invoice-list/invoice-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Invoice } from '../../../models/invoice';
 import { loadAllInvoices } from '../actions/invoices.actions';
@@ -14,6 +15,15 @@ import { InvoiceState, selectAll } from '../reducers/invoices.reducer';
 export class InvoiceListComponent implements OnInit {
   isDarkMode$!: Observable<boolean>;
   invoices$!: Observable<Invoice[]>;
+  filteredInvoices$!: Observable<Invoice[]>;
+  dropdownVisible = false;
+  dropdownTimeoutId: any;
+
+  selectedStatuses: { [key: string]: boolean } = {
+    pending: false,
+    paid: false,
+    draft: false,
+  };
 
   constructor(
     private store: Store<{
@@ -27,5 +37,38 @@ export class InvoiceListComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(loadAllInvoices());
+
+    this.filteredInvoices$ = this.invoices$;
+  }
+
+  showDropdown(): void {
+    clearTimeout(this.dropdownTimeoutId);
+    this.dropdownVisible = true;
+  }
+
+  hideDropdownWithDelay(): void {
+    this.dropdownTimeoutId = setTimeout(() => {
+      this.dropdownVisible = false;
+    }, 300);
+  }
+
+  toggleStatus(status: string): void {
+    this.selectedStatuses[status] = !this.selectedStatuses[status];
+
+    this.filteredInvoices$ = combineLatest([this.invoices$]).pipe(
+      map(([invoices]) => {
+        const selectedStatuses = Object.keys(this.selectedStatuses).filter(
+          (key) => this.selectedStatuses[key]
+        );
+
+        if (selectedStatuses.length === 0) {
+          return invoices;
+        }
+
+        return invoices.filter((invoice) =>
+          selectedStatuses.includes(invoice.status.toLowerCase())
+        );
+      })
+    );
   }
 }
